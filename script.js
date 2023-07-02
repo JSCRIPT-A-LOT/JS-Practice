e9d33a9d896b3745d1dfebe4850ff41d11357949
JSCRIPT-A-LOT <jscript.a.lot@gmail.com>
2023-07-02 17:40:13 -0600

    add comments with instructions for each question

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// will be using q1 ,q2 ,q3 ... and so on for each question's answer starting with the instructions commented above each question
+// Write a function called add7 that takes one number and returns that number + 7.
 function add7(n) {
 return n +7;
 }
-let answer = add7(37281);
-console.log(answer);
+let q1 = add7(37281);
+console.log(q1);
+
+// Write a function called multiply that takes 2 numbers and returns their product.
+function multiply(a, b) {
+    return a * b;
+}
+let q2 
