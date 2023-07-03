86fa6a70b4494e6b471acecd09195a422ad3f177
JSCRIPT-A-LOT <jscript.a.lot@gmail.com>
2023-07-03 13:03:03 -0600

    create function to convert computer choice to valid value

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -5,7 +5,20 @@ const three = "scissors"
 // generates random # form 1-3
 const computer = () => {
     let turn = (Math.floor(Math.random() * 3 ) + 1);
-    console.log(turn);
+    return turn;
 }
-computer();
-
+function convertComputer(computer){
+if (computer === 1) {
+    computer = one;
+    console.log("one")
+}
+else if (computer === 2) {
+    computer = two;
+    console.log("two")
+}
+else if (computer === 3) {
+    computer = three;
+    console.log("three")
+}
+}
+convertComputer();
